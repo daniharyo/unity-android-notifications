2f481b4f984bd9cbd2c698a0bd1ed5d5ec6352fe
Dani Haryo <dani@touchten.com>
2016-05-27 14:19:07 +0700

    - fix ios function to cancel specified notification

--- LocalNotifTest/Assets/Scripts/LocalNotification/IOSLocalNotification.cs
+++ LocalNotifTest/Assets/Scripts/LocalNotification/IOSLocalNotification.cs
@@ -5,11 +5,13 @@ using System.Collections.Generic;
 
 #if UNITY_IOS
 using Notification = UnityEngine.iOS.NotificationServices;
+using iOSLocalNotification = UnityEngine.iOS.LocalNotification;
 #endif
 public class IOSLocalNotification : ILocalNotification
 {
 
 	List<NotificationModel> notificationList;
+	private const string UserInfoIDKey = "id";
 
 	public IOSLocalNotification ()
 	{
@@ -45,13 +47,15 @@ public class IOSLocalNotification : ILocalNotification
 	public void Dispatch ()
 	{
 		#if UNITY_IOS
-		foreach(NotificationModel model in notificationList)
-		{
-			var notif = new UnityEngine.iOS.LocalNotification();
+		foreach (NotificationModel model in notificationList) {
+			var notif = new UnityEngine.iOS.LocalNotification ();
 			notif.fireDate = model.Time;
 			notif.alertAction = model.title;
 			notif.alertBody = model.message;
-			Notification.ScheduleLocalNotification(notif);
+			Dictionary<string,string> info = new Dictionary<string,string> ();
+			info.Add (UserInfoIDKey, model.id.ToString ());
+			notif.userInfo = info;
+			Notification.ScheduleLocalNotification (notif);
 		}
 
 		notificationList = new List<NotificationModel> ();
@@ -60,18 +64,50 @@ public class IOSLocalNotification : ILocalNotification
 
 	public void CancelScheduledNotification (int id)
 	{
+		
 		#if UNITY_IOS
-		Notification.CancelLocalNotification(Notification.GetLocalNotification(id));
+
+		int notifID = getNotificationIndex (id);
+		if (notifID == -1) {
+			return;
+		}
+		Notification.CancelLocalNotification (Notification.GetLocalNotification (notifID));
 		#endif
 	}
 
 	public void CancelAllScheduledNotification ()
 	{
 		#if UNITY_IOS
-		Notification.CancelAllLocalNotifications();
+		Notification.CancelAllLocalNotifications ();
 		#endif
 	}
 
 	#endregion
-	
+
+	#if UNITY_IOS
+	private int getNotificationIndex (int id)
+	{
+		for (int i = 0; i < Notification.scheduledLocalNotifications.Length; i++) {
+			if (isIDEqual (Notification.scheduledLocalNotifications [i], id)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	private bool isIDEqual (iOSLocalNotification notif, int id)
+	{
+		string notifID = string.Empty;
+		Dictionary<string,string> userInfo = (Dictionary<string,string>)notif.userInfo;
+
+		if (userInfo.TryGetValue (UserInfoIDKey, out notifID)) {
+			if (notifID == id.ToString ()) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		return false;
+	}
+	#endif
 }
